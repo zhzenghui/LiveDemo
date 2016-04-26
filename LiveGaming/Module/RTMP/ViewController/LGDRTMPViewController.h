9a1f7a67931891cab32ca3ae5b3f6ca7545b46ef
zenghui <zhzenghui@gmail.com>
2016-04-26 16:28:46 +0800

    载入ijk推流播放器

---
+++ LiveGaming/Module/RTMP/ViewController/LGDRTMPViewController.h
@@ -0,0 +1,15 @@
+//
+//  LGDViewController.h
+//  LiveGaming
+//
+//  Created by xy on 16/4/25.
+//  Copyright © 2016年 zenghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGDRTMPViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *openSetting;
+
+@end
---
+++ LiveGaming/Module/RTMP/ViewController/LGDRTMPViewController.m
@@ -0,0 +1,31 @@
+//
+//  LGDViewController.m
+//  LiveGaming
+//
+//  Created by xy on 16/4/25.
+//  Copyright © 2016年 zenghui. All rights reserved.
+//
+
+#import "LGDRTMPViewController.h"
+
+#import "IJKMoviePlayerViewController.h"
+
+
+
+@interface LGDRTMPViewController ()
+@end
+
+@implementation LGDRTMPViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+@end
