65467414f2a69b438d5b98c10a325a3934ebcbd9
zenghui <zhzenghui@gmail.com>
2016-04-26 15:28:46 +0800

    推流

--- LiveGaming/Module/Main/ViewController/LGDViewController.m
+++ LiveGaming/Module/Main/ViewController/LGDViewController.m
@@ -22,8 +22,8 @@
     self.title = @"主页";
     self.titles = @[].mutableCopy;
     self.classNames = @[].mutableCopy;
-    [self addCell:@"界面流畅优化" class:@"SmoothInterface"];
-    [self addCell:@"FFMpeg视频播放器" class:@"LGDPlayerViewController"];
+    [self addCell:@"1.界面流畅优化" class:@"SmoothInterface"];
+    [self addCell:@"2.FFMpeg视频播放器" class:@"LGDPlayerViewController"];
 //    [self addCell:@"Image" class:@"YYImageExample"];
 //    [self addCell:@"Text" class:@"YYTextExample"];
 //    //    [self addCell:@"Utility" class:@"YYUtilityExample"];
---
+++ LiveGaming/Module/推流/ViewController/LGDStreamerViewController.m
@@ -0,0 +1,27 @@
+//
+//  LGDViewController.m
+//  LiveGaming
+//
+//  Created by xy on 16/4/25.
+//  Copyright © 2016年 zenghui. All rights reserved.
+//
+
+#import "LGDStreamerViewController.h"
+
+@interface LGDStreamerViewController ()
+@end
+
+@implementation LGDStreamerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+@end
